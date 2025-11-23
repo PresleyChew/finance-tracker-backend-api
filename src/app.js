5b043c9e916cde74836b7b7af8c262dffe98e078
Presley <renren42534@gmail.com>
2025-11-23 19:37:13 +0800

    Implemented full authentication

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 import express from 'express'
+import dotenv from 'dotenv';
+import authRouter from './routes/authRouter.js'
+import accountRouter from './routes/accountRouter.js'
+import {authVerifier} from './middleware/authMiddleware.js'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use('/auth',authRouter)
+app.use('/accounts', authVerifier, (req,res) => {
+    console.log('all good')
+})
+
+app.listen(PORT, (req,res) => {
+    console.log(`Listening at: ${PORT}`)
+})
---
+++ src/controllers/authController.js
@@ -0,0 +1,56 @@
+import prisma from '../utils/prismaClient.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export async function register(req,res) {
+    const {username,password} = req.body
+    try {
+        // frontend assumes username and password exists else frontend will return error
+        // we check if username exist in db first we dont want to register existing clients
+        const exist = await prisma.user.findUnique({
+            where: {
+                username : username
+            }
+        })
+        if (exist) {
+            return res.status(409).json({error:'User already exists'})
+        }
+        const hashedPassword = await bcrypt.hash(password,9)
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password:hashedPassword
+            }
+        })
+        const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'24h'})
+        res.status(201).json({message:'User created successfully',token:token})
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+export async function login(req,res) {
+    const {username,password} = req.body
+    try{
+        const exist = await prisma.user.findUnique({
+            where: {
+                username:username
+            }
+        })
+        if (!exist) { // return generic error msg so client wont know internal state
+            return res.status(401).json({error:'Invalid Credentials'})
+        }
+        // else means exist we can compare
+        const isVerified = await bcrypt.compare(password,exist.password)
+        if (!isVerified) {
+            return res.status(401).json({error:'Invalid credentials'})
+        }
+        const token = jwt.sign({id:exist.id},process.env.JWT_SECRET,{expiresIn:'24h'})
+        res.status(200).json({message:'User logged in successfully',token:token})
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
