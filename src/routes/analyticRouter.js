f705632b62e874d82eb790adbd9ac4695c1232cd
Presley <renren42534@gmail.com>
2025-11-25 16:15:15 +0800

    Boilerplate for analytics API

---
+++ src/routes/analyticRouter.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import analyticControllers from '../controllers/analyticController.js'
+
+const router = express.Router()
+
+//ENDPOINTS FOR ANALYTICS
+
+export default router
