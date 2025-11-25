1bf772101f9322b01658a78192f61b61c5bb46c4
Presley <renren42534@gmail.com>
2025-11-25 16:10:15 +0800

    Included Transaction CRUD

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,7 +1,127 @@
 import prisma from '../utils/prismaClient.js'
 
+async function getAll(req,res) {
+    const userId = req.userId
+    try {
+        const transactions = await prisma.transaction.findMany({
+            where: {userId}
+        })
+        res.status(200).json(transactions)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
 
+async function getIndiv(req,res) {
+    const userId = req.userId
+    const id = Number(req.params.id)
+    if (!id) {
+        return res.status(400).json({error:'Invalid ID Provided'})
+    }
+    try {
+        const indivTransaction = await prisma.transaction.findFirst({
+            where: {id, userId}
+        })
+        if (!indivTransaction) {
+            return res.status(404).json({error:'Transaction not found or unauthorized'})
+        }
+        res.status(200).json(indivTransaction)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+async function createTransaction(req,res) {
+    const {desc,cost,accountId} = req.body
+    const userId = req.userId
+    try {
+        const account = await prisma.account.findFirst({
+            where: {id:accountId,userId}
+        })
+        if (!account) {
+            return res.status(404).json({error:'Account provided is incorrect or does not belong to requester'})
+        }
+        const transaction = await prisma.transaction.create({
+            data: {
+                desc,
+                cost,
+                userId,
+                accountId
+            }
+        })
+        res.status(201).json(transaction)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+async function deleteTransaction(req,res) {
+    const userId = req.userId
+    const id = Number(req.params.id)
+    if (!id) {
+        return res.status(400).json({error:'Invalid ID Provided'})
+    }
+    try {
+        const itemToBeDeleted = await prisma.transaction.findFirst({
+            where: { id, userId}
+        })
+        if (!itemToBeDeleted) {
+            return res.status(404).json({error:'Transaction not found or unauthorized'})
+        }
+        const deletedItem = await prisma.transaction.delete({
+            where: {id}
+        })
+        res.status(200).json(deletedItem)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+async function editTransaction(req,res) {
+    const userId = req.userId
+    const id = Number(req.params.id)
+    const {desc,cost,accountId} = req.body
+    if (!id) {
+        return res.status(400).json({error:'Invalid ID Provided'})
+    }
+    try {
+        const itemToBeUpdated = await prisma.transaction.findFirst({
+            where:{id,userId}
+        })
+        if (!itemToBeUpdated) {
+            return res.status(404).json({error:'Transaction not found or unauthorized'})
+        }
+        if (accountId) {
+            const account = await prisma.account.findFirst({
+                where:{id:accountId,userId}
+                })
+            if(!account) {
+                return res.status(404).json({error:'Account provided is incorrect or does not belong to requestor'})
+            }
+        }
+        const updatedItem = await prisma.transaction.update({
+            where: {id},
+            data: {
+                desc: desc ?? itemToBeUpdated.desc,
+                cost: cost ?? itemToBeUpdated.cost,
+                accountId: accountId ?? itemToBeUpdated.accountId
+            }
+        })
+        res.status(200).json(updatedItem)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
 
 export default {
-    
+    getAll,
+    getIndiv,
+    createTransaction,
+    deleteTransaction,
+    editTransaction
 }
--- src/routes/transactionRouter.js
+++ src/routes/transactionRouter.js
@@ -3,4 +3,11 @@ import transactionControllers from '../controllers/transactionController.js'
 
 const router = express.Router()
 
+
+router.get('/',transactionControllers.getAll)
+router.get('/:id',transactionControllers.getIndiv)
+router.post('/',transactionControllers.createTransaction)
+router.delete('/:id',transactionControllers.deleteTransaction)
+router.patch('/:id',transactionControllers.editTransaction)
+
 export default router
