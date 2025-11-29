5d17a75b881f4ebce5c5bf7d0d84677196a1ef46
Presley <renren42534@gmail.com>
2025-11-29 15:42:36 +0800

    Complete aggregate features

--- src/controllers/analyticController.js
+++ src/controllers/analyticController.js
@@ -2,6 +2,7 @@ import prisma from '../utils/prismaClient.js'
 
 async function spentPerAccount(req,res) {
     const userId = req.userId
+    const months = Number(req.query.months) || 12
     try {
         const results = await prisma.$queryRaw`
         SELECT
@@ -11,6 +12,7 @@ async function spentPerAccount(req,res) {
         JOIN "Transaction" t
         ON t."accountId" = a.id
         WHERE a."userId" = ${userId}
+        AND "createdAt" >= NOW() - (${months} || ' months')::interval
         GROUP BY a.bank
         `
         res.status(200).json(results)
@@ -20,27 +22,42 @@ async function spentPerAccount(req,res) {
     }
 }
 
-async function spentPerCategory(req,res) {
-    const userId = req.userId
+async function spentPerCategory(req, res) {
+    const userId = req.userId;
+    const months = Number(req.query.months) || 1
+
+    const endDate = new Date()
+    const startDate = new Date()
+    startDate.setMonth(startDate.getMonth() - months)
     try {
         const results = await prisma.transaction.groupBy({
-            by:['category'],
-            where: {userId},
-            _sum: {cost:true}
+        by: ['category'],
+        where: {
+            userId,
+            createdAt: {
+            gte: startDate,
+            lt: endDate
+            }
+        },
+        _sum: { cost: true }
         })
+
         const formatted = results.map(r => ({
-            category: r.category,
-            cost: r._sum.cost
-        }));
+        category: r.category,
+        totalSpent: r._sum.cost
+        }))
+
         res.status(200).json(formatted)
-    }catch(err) {
+    } catch (err) {
         console.log(err)
-        res.status(500).json({error:'Internal Server Error'})    
+        res.status(500).json({ error: 'Internal Server Error' })
     }
 }
 
+
 async function totalSpentBySpecificAcc(req,res) {
     const userId = req.userId
+    const months = Number(req.query.months) || 12
     const id = Number(req.params.id)
     try {
         const result = await prisma.$queryRaw`
@@ -50,7 +67,7 @@ async function totalSpentBySpecificAcc(req,res) {
         FROM "Account" a
         JOIN "Transaction" t
         ON t."accountId" = a.id
-        WHERE t."userId" = ${userId} AND t."accountId" = ${id}
+        WHERE t."userId" = ${userId} AND t."accountId" = ${id} AND "createdAt" >= NOW() - (${months} || ' months')::interval
         GROUP BY BANK
         `
         res.status(200).json(result)
@@ -59,8 +76,89 @@ async function totalSpentBySpecificAcc(req,res) {
         res.status(500).json({error:'Internal Server Error'})
     }
 }
+
+async function totalSpent(req,res) {
+    try {
+        const userId = req.userId
+        const months = Number(req.query.months) || 12
+        const startDate = new Date()
+        const endDate = new Date()
+        startDate.setMonth(startDate.getMonth()-months)
+        const result = await prisma.transaction.aggregate({
+            where:{
+                userId:userId,
+                createdAt:{
+                    gte:startDate,
+                    lt:endDate
+                }
+            },
+            _sum:{cost:true}
+        })
+        res.status(200).json({totalSpent:result._sum.cost || 0})
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+async function topCategories(req,res) {
+    try {
+        const userId = req.userId
+        const months = Number(req.query.months) || 12
+        const endDate = new Date()
+        const startDate = new Date()
+        startDate.setMonth(startDate.getMonth()-months)
+        const result = await prisma.transaction.groupBy({
+            by:['category'],
+            where:{
+                userId:userId,
+                createdAt:{
+                    gte:startDate,
+                    lt:endDate
+                }
+            },
+            _sum:{cost:true},
+            orderBy: {
+                _sum:{cost:'desc'}
+            }
+        })
+        const finalResult = result.map(item => ({
+            category:item.category,
+            totalSpent:item._sum.cost
+        }))
+        res.status(200).json(finalResult)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
+
+async function accountRankings(req,res) {
+    try {
+        const userId = req.userId
+        const months = Number(req.query.months) || 12
+        const result = await prisma.$queryRaw`
+        SELECT
+        a.bank AS bank,
+        SUM(t.cost) AS "totalSpent"
+        FROM "Account" a
+        JOIN "Transaction" t
+        ON t."accountId" = a.id
+        WHERE a."userId" = ${userId} AND t."createdAt" >= NOW() - (${months} || ' months')::interval
+        GROUP BY a.bank
+        ORDER BY "totalSpent" DESC
+        `
+        res.status(200).json(result)
+    }catch(err) {
+        console.log(err)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+}
 export default {
     spentPerAccount,
     spentPerCategory,
-    totalSpentBySpecificAcc
+    totalSpentBySpecificAcc,
+    totalSpent,
+    topCategories,
+    accountRankings
 }
