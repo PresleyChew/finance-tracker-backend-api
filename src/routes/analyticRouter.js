60a8bc000d0655b45cb02f62c25393c78bec8ff4
Presley <renren42534@gmail.com>
2025-11-29 15:42:36 +0800

    Complete aggregate features

--- src/routes/analyticRouter.js
+++ src/routes/analyticRouter.js
@@ -3,11 +3,17 @@ import analyticControllers from '../controllers/analyticController.js'
 
 const router = express.Router()
 
-// finds how much per bank for this userID
+
 router.get('/spent-per-acc',analyticControllers.spentPerAccount)
-// finds how much per category for userID
+
 router.get('/spent-per-category',analyticControllers.spentPerCategory)
-// finds how much spent by a specific account
+
 router.get('/acc/:id/total-spent',analyticControllers.totalSpentBySpecificAcc)
+
+router.get('/total-spent',analyticControllers.totalSpent)
+
+router.get('/top-categories',analyticControllers.topCategories)
+
+router.get('/acc-ranking',analyticControllers.accountRankings)
 
 export default router
